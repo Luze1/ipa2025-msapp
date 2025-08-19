88faf994b4465a40765d2ff2190c8852a52e8955
Luze1 <66070107@kmitl.ac.th>
2025-08-19 17:23:18 +0700

    ipa_lab_docker

---
+++ ipa2025web/sample-app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip3 install flask pymongo bson" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t web .
+
+docker run -t -d -p 8080:8080 --name web web
+
+docker ps -a
