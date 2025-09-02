3f04ec9375480d750f48f5d40fd6aa58790e4cdb
Luze1 <66070107@kmitl.ac.th>
2025-09-02 15:59:13 +0700

    fix prep-8-01

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -2,6 +2,7 @@ import pika
 import os
 
 def produce(host, body):
+
     rabbitmq_user = os.environ.get("RABBITMQ_DEFAULT_USER")
     rabbitmq_password = os.environ.get("RABBITMQ_DEFAULT_PASS")
 
@@ -13,9 +14,11 @@ def produce(host, body):
 
     channel.exchange_declare(exchange="jobs", exchange_type="direct")
     channel.queue_declare(queue="router_jobs")
-    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+    channel.queue_bind(queue="router_jobs", exchange="jobs", \ 
+    routing_key="check_interfaces")
 
-    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
+    channel.basic_publish(exchange="jobs",\
+     routing_key="check_interfaces", body=body)
 
     connection.close()
 
