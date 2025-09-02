be0cd27e10b6b49cff8217eaf955b956c031d34f
Luze1 <66070107@kmitl.ac.th>
2025-09-02 16:08:29 +0700

    clean code

--- web/app.py
+++ web/app.py
@@ -41,7 +41,6 @@ def add_comment():
 
 @sample.route("/router/<ip>")
 def handle_ip(ip):
-    # Find the latest 3 router records for the selected IP, sorted by timestamp descending
     routers = list(myint.find({"router_ip": ip}).sort("timestamp", -1).limit(3))
     html = """
     <html>
